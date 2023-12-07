05c1138c439da45a224664243d804281a15dd6c8
Cameron McNiff <cmcn38@gmail.com>
2023-12-07 12:32:58 +0000

    Added Day 7, part 2

--- src/07.py
+++ src/07.py
@@ -19,35 +19,59 @@ def parse_hand(string):
     bet = int(re.search(r'[0-9]+$', string).group())
     return hand, bet
 
-def check_hand(string):
-    count = Counter(string)
-    values = sorted(count.values(), reverse = True)
+def check_hand(string, part):
+    if part == 1:
+        count = Counter(string)
+        values = sorted(count.values(), reverse = True)
 
-    if 5 in values:
-        return 7
-    elif 4 in values:
-        return 6
-    elif 3 in values and 2 in values:
-        return 5
-    elif 3 in values:
-        return 4
-    elif values.count(2) == 2:
-        return 3
-    elif 2 in values:
-        return 2
-    else:
-        return 1 
+        if 5 in values:
+            return 7
+        elif 4 in values:
+            return 6
+        elif 3 in values and 2 in values:
+            return 5
+        elif 3 in values:
+            return 4
+        elif values.count(2) == 2:
+            return 3
+        elif 2 in values:
+            return 2
+        else:
+            return 1 
+    if part == 2:
+        wildcards = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
+        hands = []
+    
+        for wildcard in wildcards:
+            modified_hand = [card if card != 'J' else wildcard for card in string]
+            count = Counter(modified_hand)
+            values = sorted(count.values(), reverse = True)
 
-def get_rank(string):
+            if 5 in values:
+                hands.append(7)
+            elif 4 in values:
+                hands.append(6)
+            elif 3 in values and 2 in values:
+                hands.append(5)
+            elif 3 in values:
+                hands.append(4)
+            elif values.count(2) == 2:
+                hands.append(3)
+            elif 2 in values:
+                hands.append(2)
+            else:
+                hands.append(1)
+        return max(hands)
+
+
+ 
+def get_rank(string, part):
     hands_rank = []
     for index, i in enumerate(string.splitlines()):
         hand, bet = parse_hand(i)
-        hand_res = check_hand(hand)
+        hand_res = check_hand(hand, part)
         hands_rank.append([hand, bet, hand_res])
     
-    #char_ranks = {'2': 12, '3': 11, '4': 10, '5': 9, '6': 8, '7': 7, '8': 6, '9': 5, 'T': 4, 'J': 3, 'Q': 2, 'K': 1, 'A': 0}
-    char_ranks = {'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, 'T': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12}
-   
     hands_rank.sort(key=lambda x: (x[2], [char_ranks[c] for c in x[0]]))
 
     rank = 0
@@ -65,7 +89,20 @@ def calc_winnings(list):
         total += sublist[4]
     return total
     
-result = get_rank(input_str)
+char_ranks = {'2': 0, '3': 1, '4': 2, '5': 3, '6': 4, '7': 5, '8': 6, '9': 7, 'T': 8, 'J': 9, 'Q': 10, 'K': 11, 'A': 12}
+
+result = get_rank(input_str, 1)
 total_win = calc_winnings(result)
 
 print(f'Part 1 Answer: {total_win}')
+
+## Part 2
+
+char_ranks = {'2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, 'T': 9, 'J': 0, 'Q': 10, 'K': 11, 'A': 12}
+
+result = get_rank(input_str, 2)
+total_win = calc_winnings(result)
+
+print(f'Part 2 Answer: {total_win}')
+
+
