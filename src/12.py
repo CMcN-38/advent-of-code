2e874ae43fc1d5d5c460335cb88d4b721678ee89
Cameron McNiff <cmcn38@gmail.com>
2024-01-01 16:43:10 +0000

    Finished day 12 with help

--- src/12.py
+++ src/12.py
@@ -75,50 +75,86 @@ for i, line in enumerate(springs):
 print(f'Part 1 Answer = {result}')
 
 # Part 2:
+# The following is attempting to memoise my above code but I cannot work it. The correct solution below is following a guide by HyperNeutrino
 
-def generate_combinations_2(spring, guide, index=0, memo = None):
-    if memo is None:
-        memo = {}
+# def generate_combations_2(spring, guide, index=0, memo = None):
+#     if memo is None:
+#         memo = {}
 
-    if index == len(spring):
-        if check_pattern(spring, guide):
-            return 1
-        return 0
+#     if index == len(spring):
+#         if check_pattern(spring, guide):
+#             return 1
+#         return 0
+    
+#     if (spring, index) in memo:
+#         return memo[(spring, index)]
 
-    if (spring, index) in memo:
-        return memo[(spring, index)]
+#     possible = 0
+#     if spring[index] == '?':
+#         spring_dot = spring[:index] + '.' + spring[index + 1:]
+#         possible += generate_combinations_2(spring_dot, guide, index + 1, memo)
 
-    possible = 0
-    if spring[index] == '?':
-        spring_dot = spring[:index] + '.' + spring[index + 1:]
-        possible += generate_combinations_2(spring_dot, guide, index + 1, memo)
+#         spring_hash = spring[:index] + '#' + spring[index + 1:]
+#         possible += generate_combinations_2(spring_hash, guide, index + 1, memo)
+#     else:
+#         possible += generate_combinations_2(spring, guide, index + 1, memo)
 
-        spring_hash = spring[:index] + '#' + spring[index + 1:]
-        possible += generate_combinations_2(spring_hash, guide, index + 1, memo)
-    else:
-        possible += generate_combinations_2(spring, guide, index + 1, memo)
-
-    memo[(spring, index)] = possible
-
-
-    return possible
+#     memo[(spring, index)] = possible
 
 
+#     return possible
+
+# new_springs = []
+# for spring in springs:
+#    new_spring = '?'.join([spring] * 5) 
+#    new_springs.append(new_spring)
+
+# new_guides = []
+# for guide in guides:
+#     new_guide = guide * 5
+#     new_guides.append(new_guide)
+
+# result = 0
+# total = 0
+# for i, line in enumerate(new_springs):
+#     result = generate_combinations_2(line, new_guides[i])
+#     total += result    
+# print(f'Part 2 Answer = {result}, or {total}')
+memo = {}
+
+def count(spring, guide):
+    if spring == "":
+        return 1 if guide == () else 0
+
+    if guide == ():
+        return 0 if "#" in spring else 1
+
+    memo_key = (spring, guide)
+
+    if memo_key in memo:
+        return memo[memo_key]
+
+    result = 0
+    
+    if spring[0] == "." or spring[0] == "?":
+        result += count(spring[1:], guide)
+
+    if spring[0] == "#" or spring[0] == "?":
+        if guide[0] <= len(spring) and "." not in spring[:guide[0]] and (guide[0] == len(spring) or spring[guide[0]] != "#"):
+            result += count(spring[guide[0] + 1:], guide[1:])
+
+    memo[memo_key] = result
+    return result
+
+total = 0
+
+for line in run_str.splitlines():
+    spring, guide = line.split()
+    guide = tuple(map(int, guide.split(",")))
+
+    spring = "?".join([spring] * 5)
+    guide = guide * 5
+    total += count(spring, guide)
 
 
-new_springs = []
-for spring in springs:
-   new_spring = '?'.join([spring] * 5) 
-   new_springs.append(new_spring)
-
-new_guides = []
-for guide in guides:
-    new_guide = guide * 5
-    new_guides.append(new_guide)
-
-result = 0
-
-for i, line in enumerate(new_springs):
-    result = generate_combinations_2(line, new_guides[i])
-    total += result    
-print(f'Part 2 Answer = {result}, or {total}')
+print(f'Part 2 Answer= {total}')
