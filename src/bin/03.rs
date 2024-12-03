748ecdc1883decf0018a303e1a4889d0109b1a4e
Cameron McNiff <>
2024-12-03 08:26:20 +0000

    Start Day 03

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,10 +1,25 @@
+use regex::Regex;
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut total = 0;
+
+    for line in input.lines() {
+        for captures in
+        if let Some(captures) = re.captures(line) {
+            let num1: i32 = captures[1].parse().unwrap();
+            let num2: i32 = captures[2].parse().unwrap();
+            println!("{}", num1);
+            println!("{}", num2);
+            total += num1 * num2;
+        }
+    }
+
+    Some(total as u32)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(_input: &str) -> Option<u32> {
     None
 }
 
@@ -15,7 +30,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(161));
     }
 
     #[test]
